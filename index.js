b6c4fde6bc7edb1ecf0fa59fa6482c519c3c4b4f
Hassan Aly <surel@hasssan.com>
2015-10-01 00:51:57 +0700

    Add static index page

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-var app = require('koa')();
+const path = require('path');
+const app = require('koa')();
+const serve = require('koa-static');
 
-app.use(function* () {
-  const req = this.request;
-  const path = req.path;
-  const type = req.type;
-  const charset = req.charset;
-  const query = req.query;
-  const ip = req.ip;
-
-  this.body = `
-    <html>
-    <title>POC Koa+React</title>
-    <body>
-      path: ${path}<br />
-      query: ${query.query}<br />
-      type: ${type}<br />
-      charset: ${charset}<br />
-      ip: ${ip}<br />
-    </body>
-    </html>
-  `;
-});
+app.use(serve(path.resolve(__dirname, 'src')));
 
 app.listen(3000);
 console.log('listening http://localhost:3000/');
