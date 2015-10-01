88aec38e1024d25042839b27e596a78d755f3d9f
Hassan Aly <surel@hasssan.com>
2015-10-01 12:51:13 +0700

    Add counter sample and bootstrap css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
 const App = React.createClass({
+  getInitialState() {
+    return {
+      counter: 0
+    };
+  },
+  onClick() {
+    const counter = this.state.counter + 1;
+    this.setState({counter: counter})
+  },
   render() {
     return (
-      <div>react</div>
+      <div className="container">
+        <h1>React Apps</h1>
+        <div className="row">
+          <div className="col-sm-12">
+            <h1>{this.state.counter}</h1>
+            <button type="button" onClick={this.onClick} className="btn btn-default">
+              Counter
+            </button>
+          </div>
+        </div>
+      </div>
     )
   }
 });
